bc29221a260d14a0561fdbf1a04a06fe6f8036df
FoNiX <rogueman2018@gmail.com>
2024-06-27 20:57:07 +0100

    Update comments for styles ignore

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,13 @@
 export default class Airport {
   constructor(name, code) {
+    // eslint-disable-next-line no-underscore-dangle
     this._name = name;
+    // eslint-disable-next-line no-underscore-dangle
     this._code = code;
   }
 
   get [Symbol.toStringTag]() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._code;
   }
 }
