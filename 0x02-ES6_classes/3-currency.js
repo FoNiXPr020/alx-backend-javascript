8064c74c0775fd9c4b174481b0e4d8bb457c6814
FoNiX <rogueman2018@gmail.com>
2024-06-27 19:02:08 +0100

    Fixing styles of Eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,43 +1,43 @@
 // Methods, static methods, computed methods names..... MONEY
 export default class Currency {
-    constructor(code, name) {
-        Currency._validateCode(code);
-        Currency._validateName(name);
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    Currency._validateCode(code);
+    Currency._validateName(name);
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(code) {
-        Currency._validateCode(code);
-        this._code = code;
-    }
+  set code(code) {
+    Currency._validateCode(code);
+    this._code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(name) {
-        Currency._validateName(name);
-        this._name = name;
-    }
+  set name(name) {
+    Currency._validateName(name);
+    this._name = name;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 
-    static _validateCode(code) {
-        if (typeof code !== 'string' || code.trim() === '' || !/^[A-Z$]+$/.test(code)) {
-            throw new TypeError('Code must be a non-empty string containing uppercase letters or $ symbol');
-        }
+  static _validateCode(code) {
+    if (typeof code !== 'string' || code.trim() === '' || !/^[A-Z$]+$/.test(code)) {
+      throw new TypeError('Code must be a non-empty string containing uppercase letters or $ symbol');
     }
+  }
 
-    static _validateName(name) {
-        if (typeof name !== 'string' || name.trim() === '') {
-            throw new TypeError('Name must be a non-empty string');
-        }
+  static _validateName(name) {
+    if (typeof name !== 'string' || name.trim() === '') {
+      throw new TypeError('Name must be a non-empty string');
     }
+  }
 }
